ad153b4eb57cced45db999f37a1b27a8af572899
leila ben chaalia <119710623+leilatest@users.noreply.github.com>
2023-05-08 15:05:26 +0100

    clickPharma

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const Port = 5000 || process.env.Port;
+app.listen(Port, (err) => {
+  if (err) throw err;
+  console.log("server is running");
+});
